5bf96bc1c2a9caa2d4d63f92b14e5d749fe4cbe8
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 18:23:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -58,6 +58,11 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   <<-SQL
-  
+  SELECT chararcters.name, COUNT(books.id)
+  FROM characters
+  JOIN character_books
+  ON books.id = character_books.book_id
+  GROUP BY characters.name
+  ORDER BY COUNT(books.id) DESC, characters.name ASC
   SQL
 end
