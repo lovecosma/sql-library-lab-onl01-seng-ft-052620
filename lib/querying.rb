bbefdd4b5062f9ae06f8e718178b63393e88fbbe
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 17:35:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -47,7 +47,7 @@ def select_series_title_with_most_human_characters
   JOIN authors
   ON series.author_id = authors.id
   JOIN characters
-  ON characters.author_id = seires.author_id
+  ON characters.author_id = series.author_id
   SQL
 end
 
