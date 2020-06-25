420c5e68c77c32d4e466037d6515b5cde776adaa
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 17:05:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,18 +1,43 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  <<-SQL
+  SELECT books.title, books.year
+  FROM books
+  JOIN series
+  ON books.series_id = series.id
+  WHERE series.id = 1
+  ORDER BY books.year;
+  SQL
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  <<-SQL
+  SELECT characters.name, characters.motto
+  FROM characters
+  ORDER BY LENGTH(characters.motto) DESC
+  LIMIT 1;
+  SQL
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  <<-SQL
+  SELECT characters.species, COUNT(characters.id)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY COUNT(characters.id) DESC
+  LIMIT 1;
+  SQL
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  <<-SQL
+  SELECT author.name, subgenres.name
+  FROM authors
+  JOIN subgenres
+  ON series.subgenres_id =
+  ORDER BY LENGTH(characters.motto) DESC
+  LIMIT 1;
+  SQL
 end
 
 def select_series_title_with_most_human_characters
---
+++ lib/update.sql
@@ -0,0 +1,6 @@
+UPDATE characters
+SET species = "Martian"
+WHERE id = (
+  SELECT MAX(id)
+  FROM characters
+)
