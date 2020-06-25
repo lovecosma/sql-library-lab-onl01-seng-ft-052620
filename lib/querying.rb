a9e83c70b0705a41d97a27e2a626f120c5920b92
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 17:12:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,8 +36,6 @@ def select_name_and_series_subgenres_of_authors
   JOIN subgenres
   ON authors.id = series.author_id
   WHERE series.subgenre_id = subgenres.id
-  ORDER BY LENGTH(characters.motto) DESC
-  LIMIT 1;
   SQL
 end
 
